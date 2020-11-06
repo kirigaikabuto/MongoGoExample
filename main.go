c8d83564f0493c89edfc116a29b983b65c1abcb7
YTleugazy@dar.kz <tleugazy98@gmail.com>
2020-11-06 21:28:39 +0600

    sdsdd

--- main.go
+++ main.go
@@ -8,19 +8,20 @@ import (
 	"go.mongodb.org/mongo-driver/mongo/options"
 	"log"
 )
-
+//create global variables
 var (
 	Collection     *mongo.Collection
 	Database       string = "lab3"
 	CollectionName string = "posts"
 )
 
+//create post struct
 type Post struct {
 	Id   int64  `json:"id"`
 	Name string `json:"name"`
 	Body string `json:"body"`
 }
-
+//create method to the struct of Post
 func (p *Post) Add() {
 	insertResult, err := Collection.InsertOne(context.TODO(), p)
 	if err != nil {
@@ -29,11 +30,15 @@ func (p *Post) Add() {
 	fmt.Println("Inserted post with ID:", insertResult.InsertedID)
 }
 
+//create function for getting Post by id
 func GetPostById(id int64) (*Post, error) {
+	//create filter of type Document where exists condition by id
 	filter := bson.D{
 		{"id", id},
 	}
+	//create empty post instance in order to upload to this decoded data from mongodb
 	post := &Post{}
+	//find element  by the filter and set it to post variable
 	err := Collection.FindOne(context.TODO(), filter).Decode(&post)
 	if err != nil {
 		return nil, err
@@ -42,6 +47,7 @@ func GetPostById(id int64) (*Post, error) {
 }
 
 func CreateIndex() error {
+	//create one index for name and body fields and also set options unique to true for not duplication of data
 	indexName, err := Collection.Indexes().CreateOne(
 		context.Background(),
 		mongo.IndexModel{
@@ -54,28 +60,36 @@ func CreateIndex() error {
 	if err != nil {
 		return err
 	}
-	fmt.Print("Create Index " + indexName)
+	fmt.Println("Create Index " + indexName)
 	return nil
 }
 
 func main() {
+	//create options for connection to mongodb
 	clientOptions := options.Client().ApplyURI("mongodb://localhost:27017")
+	//create connection to the mongodb
 	client, err := mongo.Connect(context.TODO(), clientOptions)
-	Collection = client.Database(Database).Collection(CollectionName)
 	if err != nil {
 		log.Fatal(err)
 	}
-	//err = CreateIndex()
-	//if err != nil {
-	//	log.Fatal(err)
-	//}
+	//select database and also collection
+	Collection = client.Database(Database).Collection(CollectionName)
+	//create index for name and body
+	err = CreateIndex()
+	if err != nil {
+		log.Fatal(err)
+	}
+	//create post instance
 	post1 := &Post{
 		4,
 		"231321321",
 		"1232131233",
 	}
+	//run add method to add post to the mongodb
 	post1.Add()
+	//get post by the id
 	currentPost, err := GetPostById(1)
-	fmt.Print(currentPost)
+	fmt.Println(currentPost)
+	//close connection after all actions will be done
 	defer client.Disconnect(context.TODO())
 }
